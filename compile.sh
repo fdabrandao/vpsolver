8839d0db9fe546d20b48694debb4affd4a2c91f5
Filipe Brandao <fdabrandao@gmail.com>
2013-11-30 22:28:08 +0000

    updated readme and compile.sh

--- compile.sh
+++ compile.sh
@@ -6,69 +6,47 @@ make clean >compile.log 2>&1
 
 echo "mandatory:"
 
-echo -n "  bin/vbp2afg"
+echo -n "  vbp2afg "
 make bin/vbp2afg >>compile.log 2>&1 
-if [[ $? == 0 ]] ; then
-    echo " [OK]"
-else    
-    echo " [Failed]"
-fi
+if [[ $? == 0 ]]; then echo "[OK]";
+else echo "[Failed]"; fi
 
-echo -n "  bin/afg2mps"
+echo -n "  afg2mps "
 make bin/afg2mps >>compile.log 2>&1
-if [[ $? == 0 ]] ; then
-    echo " [OK]"
-else    
-    echo " [Failed]"
-fi
+if [[ $? == 0 ]]; then echo "[OK]";
+else echo "[Failed]"; fi
 
-echo -n "  bin/afg2lp"
+echo -n "  afg2lp  "
 make bin/afg2lp >>compile.log 2>&1
-if [[ $? == 0 ]] ; then
-    echo " [OK]"
-else    
-    echo " [Failed]"
-fi
+if [[ $? == 0 ]]; then echo "[OK]";
+else echo "[Failed]"; fi
 
-echo -n "  bin/vbpsol"
-make bin/vbpsol >>error.log 2>&1
-if [[ $? == 0 ]] ; then
-    echo " [OK]"
-else    
-    echo " [Failed]"
-fi
+echo -n "  vbpsol  "
+make bin/vbpsol >>compile.log 2>&1
+if [[ $? == 0 ]]; then echo "[OK]";
+else echo "[Failed]"; fi
 
-echo "nonmandatory:"
+echo "optional:"
 
-echo -n "  bin/vpsolver"
+echo -n "  vpsolver "
 make bin/vpsolver >>compile.log 2>&1 
-if [[ $? == 0 ]] ; then
-    echo " [OK]"
-else    
-    echo " [Failed]"
-fi
+if [[ $? == 0 ]]; then echo "[OK]";
+else echo "[Failed]"; fi
 
-echo -n "  bin/solve_gurobi"
+echo -n "  gg_afg   "
+make bin/gg_afg >>compile.log 2>&1
+if [[ $? == 0 ]]; then echo "[OK]";
+else echo "[Failed]"; fi
+
+echo -n "  solve_gurobi "
 make bin/solve_gurobi >>compile.log 2>&1
-if [[ $? == 0 ]] ; then
-    echo " [OK]"
-else    
-    echo " [Failed]"
-fi
+if [[ $? == 0 ]]; then echo "[OK]";
+else echo "[Failed]"; fi
 
-echo -n "  bin/solve_glpk"
+echo -n "  solve_glpk   "
 make bin/solve_glpk >>compile.log 2>&1
-if [[ $? == 0 ]] ; then
-    echo " [OK]"
-else    
-    echo " [Failed]"
-fi
+if [[ $? == 0 ]]; then echo "[OK]";
+else echo "[Failed]"; fi
 
-echo -n "  bin/gg_afg"
-make bin/gg_afg >>error.log 2>&1
-if [[ $? == 0 ]] ; then
-    echo " [OK]"
-else    
-    echo " [Failed]"
-fi
+
 
