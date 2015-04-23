7ed9e565229e1ab6dd2008850866d50d94668e73
Filipe Brandao <fdabrandao@gmail.com>
2015-04-23 16:38:03 +0100

    more flexible way of adding constraints

--- pyvpsolver/model.py
+++ pyvpsolver/model.py
@@ -21,8 +21,53 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from writelp import write_lp
 from writemps import write_mps
+from collections import Iterable
 
 inf = float('inf')
+
+def transform_lincomb(left, sign, right):
+    assert type(left) != int or type(right) != int
+    lincomb = []
+    def add_entry(x, sign = 1):
+        assert type(x) in [str, tuple]
+        if type(x) == str:
+            lincomb.append((x, sign))
+        if type(x) in [tuple, list]:
+            assert len(x) == 2
+            assert type(x[0]) == int or type(x[1]) == int
+            assert type(x[0]) == str or type(x[1]) == str
+            if type(x[0]) == str:
+                lincomb.append((x[0], sign*x[1]))
+            else:
+                lincomb.append((x[1], sign*x[0]))
+
+    rhs = 0
+
+    if type(left) == int:
+        rhs -= left
+    elif type(left) == str:
+        lincomb.append((left, 1))
+    else:
+        assert isinstance(left, Iterable)
+        for x in left:
+            if type(x) not in [float, int]:
+                add_entry(x, 1)
+            else:
+                rhs -= x
+
+    if type(right) == int:
+        rhs += right
+    elif type(right) == str:
+        lincomb.append((right, -1))
+    else:
+        assert isinstance(right, Iterable)
+        for x in right:
+            if type(x) not in [float, int]:
+                add_entry(x, -1)
+            else:
+                rhs -= x
+
+    return (lincomb, sign, rhs)
 
 class Model:
     def __init__(self):
@@ -34,7 +79,7 @@ class Model:
     def setObj(self, objdir, lincomb):
         assert objdir in ["min", "max"]
         self.objdir = objdir
-        for var, coef in lincomb: 
+        for var, coef in lincomb:
             assert var in self.vars
             assert coef != inf and coef != -inf
         self.obj = lincomb
@@ -43,7 +88,7 @@ class Model:
         name = "RC%x" % len(self.cons)
         assert name not in self.cons
         return name
-        
+
     def genVarName(self):
         name = "RV%x" % len(self.vars)
         assert name not in self.vars
@@ -62,9 +107,10 @@ class Model:
         self.vars[name]["vtype"] = vtype
         return name
 
-    def addCons(self, lincomb, sign, rhs, name=None):        
+    def addCons(self, left, sign, right, name=None):
         sign = sign[:1]
         assert sign in ["<", "=", ">"]
+        lincomb, sign, rhs = transform_lincomb(left, sign, right)
         assert rhs != inf and rhs != -inf
         if lincomb == []:
             return
@@ -74,14 +120,14 @@ class Model:
             assert var in self.vars
             assert coef != inf and coef != -inf
         assert name not in self.cons
-        self.cons[name] = (lincomb, sign, rhs)       
-    
+        self.cons[name] = (lincomb, sign, rhs)
+
     def writeLP(self, lp_file):
         write_lp(self, lp_file)
-        
+
     def writeMPS(self, mps_file):
         write_mps(self, mps_file)
-        
+
     def write(self, model_file):
         if model_file.endswith(".lp"):
             self.writeLP(model_file)
@@ -89,5 +135,5 @@ class Model:
             self.writeMPS(model_file)
         else:
             raise Exception("Invalid file extension!")
-            
+
 
