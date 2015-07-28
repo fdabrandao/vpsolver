a1530ab2b5eeb0b35bdd7954e93f8f91e514a80b
Filipe Brandao <fdabrandao@gmail.com>
2015-07-28 01:53:48 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -20,43 +20,43 @@
 BASEDIR=`dirname $0`
 cd $BASEDIR
 
-make clean >compile.log 2>&1
+make clean >build.log 2>&1
 
 echo "mandatory:"
 
 echo -n "  vbp2afg "
-make bin/vbp2afg >>compile.log 2>&1
+make bin/vbp2afg >>build.log 2>&1
 if [[ $? == 0 ]]; then echo "[OK]";
 else echo "[Failed]"; fi
 
 echo -n "  afg2mps "
-make bin/afg2mps >>compile.log 2>&1
+make bin/afg2mps >>build.log 2>&1
 if [[ $? == 0 ]]; then echo "[OK]";
 else echo "[Failed]"; fi
 
 echo -n "  afg2lp  "
-make bin/afg2lp >>compile.log 2>&1
+make bin/afg2lp >>build.log 2>&1
 if [[ $? == 0 ]]; then echo "[OK]";
 else echo "[Failed]"; fi
 
 echo -n "  vbpsol  "
-make bin/vbpsol >>compile.log 2>&1
+make bin/vbpsol >>build.log 2>&1
 if [[ $? == 0 ]]; then echo "[OK]";
 else echo "[Failed]"; fi
 
 echo "optional:"
 
 echo -n "  vpsolver "
-make bin/vpsolver >>compile.log 2>&1
+make bin/vpsolver >>build.log 2>&1
 if [[ $? == 0 ]]; then echo "[OK]";
 else echo "[Failed]"; fi
 
 echo -n "  solve_gurobi "
-make bin/solve_gurobi >>compile.log 2>&1
+make bin/solve_gurobi >>build.log 2>&1
 if [[ $? == 0 ]]; then echo "[OK]";
 else echo "[Failed]"; fi
 
 echo -n "  solve_glpk   "
-make bin/solve_glpk >>compile.log 2>&1
+make bin/solve_glpk >>build.log 2>&1
 if [[ $? == 0 ]]; then echo "[OK]";
 else echo "[Failed]"; fi
