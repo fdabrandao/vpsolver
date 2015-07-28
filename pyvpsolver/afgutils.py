b091bcc7a69f09021ce8becdb278543e0174bbfe
Filipe Brandao <fdabrandao@gmail.com>
2015-07-28 18:34:09 +0100

    improve afgutils.py

--- pyvpsolver/afgutils.py
+++ pyvpsolver/afgutils.py
@@ -19,130 +19,159 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from random import *
+import random
 
-class AFGUtils:
+
+class AFGUtils(object):
+    """Tools for manipulating arc-flow graphs."""
+
     @staticmethod
     def read_graph(afg_file):
+        """Reads graphs from .afg files."""
         f = open(afg_file, "r")
         s = f.read()
 
         lst = s.split()
         m = int(lst[lst.index("M:")+1])
         lst = lst[lst.index("IDS:")+1:]
-        ids = map(int,lst[:m])
+        ids = map(int, lst[:m])
 
         s = s[s.find("#GRAPH_BEGIN#"):]
         s = s[:s.find("#GRAPH_END#\n")]
         s = s.replace("#GRAPH_BEGIN#", "")
         lst = s.split()
-        lst.pop(0) #S:
+        lst.pop(0)  # ignore "S:"
         S = int(lst.pop(0))
-        lst.pop(0) #T:
+        lst.pop(0)  # ignore "T:"
         T = int(lst.pop(0))
-        lst.pop(0) #NV:
+        lst.pop(0)  # ignore "NV:"
         NV = int(lst.pop(0))
-        lst.pop(0) #NA:
+        lst.pop(0)  # ignore "NA:"
         NA = int(lst.pop(0))
         lst = map(int, lst)
         A = []
         V = set([])
-        for i in xrange(0,len(lst),3):
+        for i in xrange(0, len(lst), 3):
             u, v, i = lst[i:i+3]
             V.add(u)
             V.add(v)
             if i < m:
-                A.append((u,v,ids[i]))
+                A.append((u, v, ids[i]))
             else:
-                A.append((u,v,i))
+                A.append((u, v, i))
         V = sorted(V)
         return V, A, S, T
 
     @staticmethod
-    def relabel(V, A, fv, fa = lambda x: x):
+    def relabel(V, A, fv, fa=lambda x: x):
+        """Relabels graphs."""
         V = map(fv, V)
         At = []
         for (u, v, i) in A:
             u, v = fv(u), fv(v)
             if u != v:
-                At.append((u,v,fa(i)))
+                At.append((u, v, fa(i)))
         return list(set(V)), list(set(At))
 
     @staticmethod
-    def draw(svg_file, V, A, multigraph=True, showlabel=False, ignore=None, loss=None):
-        if loss == None:
-            loss = [i for (u,v,i) in A if type(i)!=int]
-            loss.append(max([i for (u,v,i) in A if type(i)==int]+[-1]))
+    def draw(
+            svg_file, V, A, multigraph=True, showlabel=False, ignore=None,
+            loss=None):
+        """Draws arc-flow graphs in .svg format."""
+        if loss is None:
+            loss = [i for (u, v, i) in A if not isinstance(i, int)]
+            loss.append(max([i for (u, v, i) in A if isinstance(i, int)]+[-1]))
         try:
             from pygraphviz.agraph import AGraph
         except:
             print "Failed to load pygraphviz!"
             return
-        g = AGraph(rankdir="LR", directed=True, bgcolor="white", text="black",
-                   font_color="white", ranksep="1.0", nodesep="0.10", strict=not multigraph)
-        #g.node_attr["shape"] = "point"
+        g = AGraph(
+            rankdir="LR", directed=True, bgcolor="white", text="black",
+            font_color="white", ranksep="1.0", nodesep="0.10",
+            strict=not multigraph
+        )
+        # g.node_attr["shape"] = "point"
         g.node_attr["shape"] = "circle"
         g.node_attr["color"] = "black"
         g.node_attr["fontcolor"] = "black"
         g.node_attr["fontstyle"] = "bold"
         g.node_attr["penwidth"] = "2.0"
 
-        lbls = list(set(i for (u,v,i) in A))
+        lbls = list(set(i for (u, v, i) in A))
         lbls.sort()
         M = len(lbls)
 
         if multigraph:
             colors = Colors.uniquecolors(2*M)
-            shuffle(colors)
+            random.shuffle(colors)
 
-            for (u,v,i) in A:
-                if ignore != None and (u,v) in ignore: continue
+            for (u, v, i) in A:
+                if ignore is not None and (u, v) in ignore:
+                    continue
                 assert u != v
                 if i in loss:
-                    g.add_edge(u,v,color="black", penwidth=4)
+                    g.add_edge(u, v, color="black", penwidth=4)
                 else:
                     lbl = str(i) if showlabel else ""
-                    g.add_edge(u,v,color=colors[lbls.index(i)%len(colors)], penwidth="%d" % 4, label=lbl)
+                    g.add_edge(
+                        u, v,
+                        color=colors[lbls.index(i) % len(colors)],
+                        penwidth="{0}".format(4),
+                        label=lbl
+                    )
 
         else:
             colors = Colors.uniquecolors(M+1)
-            shuffle(colors)
+            random.shuffle(colors)
 
             links = {}
             for (u, v, i) in A:
-                if (u,v) not in links:
-                    links[u,v] = []
-                links[u,v].append(i)
+                if (u, v) not in links:
+                    links[u, v] = []
+                links[u, v].append(i)
 
             for (ind, (u, v)) in enumerate(links):
-                if ignore != None and (u,v) in ignore: continue
+                if ignore is not None and (u, v) in ignore:
+                    continue
                 assert u != v
-                if M in links[u,v] or any(type(e) != int for e in links[u,v]):
-                    g.add_edge(u,v,color="black", penwidth=4)
-                if len(links[u,v]) != 1:
-                    lbl = ",".join(map(str,links[u,v,])) if showlabel else ""
-                    g.add_edge(u,v,color=colors[ind%len(colors)], penwidth="%d" % 4, label=lbl)
+                if (
+                    M in links[u, v] or
+                    any(not isinstance(e, int) for e in links[u, v])
+                ):
+                    g.add_edge(u, v, color="black", penwidth=4)
+                if len(links[u, v]) != 1:
+                    lbl = ",".join(map(str, links[u, v])) if showlabel else ""
+                    g.add_edge(
+                        u, v,
+                        color=colors[ind % len(colors)],
+                        penwidth="{0}".format(4),
+                        label=lbl
+                    )
 
         g.draw(svg_file, format="svg", prog="dot")
-        print "SVG file '%s' generated!" % svg_file
+        print "SVG file '{0}' generated!".format(svg_file)
 
-class Colors:
+
+class Colors(object):
     """
     Finding N Distinct RGB Colors
     Based on code from StackOverflow: http://stackoverflow.com/a/2142206
     """
+
     @staticmethod
     def rgbcode(t):
+        """Converts (r, g, b) tuples to hexadecimal."""
         r, g, b = t
         r = int(r*255)
         g = int(g*255)
         b = int(b*255)
-        return '#%0.2x%0.2x%0.2x' % (r, g, b)
+        return "#{0:0>2x}{1:0>2x}{2:0>2x}".format(r, g, b)
 
     @staticmethod
     def rgbcolor(h, f, v, p):
-        """Convert a color specified by h-value and f-value to an RGB
-        three-tuple."""
+        """Converts colors specified by h-value and f-value to
+        RGB three-tuples."""
         # q = 1 - f
         # t = f
         if h == 0:
@@ -160,10 +189,13 @@ class Colors:
 
     @staticmethod
     def uniquecolors(n, v=0.5, p=0.0):
-        """Compute a list of distinct colors, ecah of which is
-        represented as an RGB three-tuple"""
+        """Computes a list of distinct colors, ecah of which is
+        represented as an RGB three-tuple."""
         import math
-        hues = (360.0 / n * i for i in range(n))
-        hs = (math.floor(hue / 60) % 6 for hue in hues)
-        fs = (hue / 60 - math.floor(hue / 60) for hue in hues)
-        return [Colors.rgbcode(Colors.rgbcolor(h, f, v, p)) for h, f in zip(hs, fs)]
+        hues = (360.0/n*i for i in range(n))
+        hs = (math.floor(hue/60) % 6 for hue in hues)
+        fs = (hue/60-math.floor(hue/60) for hue in hues)
+        return [
+            Colors.rgbcode(Colors.rgbcolor(h, f, v, p))
+            for h, f in zip(hs, fs)
+        ]
