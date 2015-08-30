326ca968d9134bda7a93de032237724bf40cae14
Filipe Brandao <fdabrandao@gmail.com>
2015-08-30 18:47:42 +0100

    Prepare release v2.0.0-rc4

--- pyvpsolver/__init__.py
+++ pyvpsolver/__init__.py
@@ -19,7 +19,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-__version__ = "v2.0.0-rc3"
+__version__ = "v2.0.0-rc4"
 
 import os
 import shutil
