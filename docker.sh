46c62f6f3a6351199b7a6a9ec9c902e1d47485b4
Filipe Brandao <fdabrandao@gmail.com>
2015-10-16 16:23:31 +0100

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -6,4 +6,4 @@ docker build -t fdabrandao/vpsolver . || exit 1
 docker stop $(docker ps -a | grep fdabrandao/vpsolver | cut -d" " -f1) 2>/dev/null
 docker rm $(docker ps -a | grep fdabrandao/vpsolver | cut -d" " -f1) 2>/dev/null
 docker run -it --rm -p 5555 fdabrandao/vpsolver || exit 1
-#docker run -it --rm -p 5555 fdabrandao/vpsolver python -m pyvpsolver.webapp.app 5555
+#docker run -it --rm -p 5555 fdabrandao/vpsolver bash
