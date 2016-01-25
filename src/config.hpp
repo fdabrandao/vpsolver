d98e9acee95c5e4c43cf7df2f1f1cd59166b2fd1
Filipe Brandao <fdabrandao@gmail.com>
2016-01-25 00:09:50 +0000

    Replace the Makefile by Autotools

---
+++ src/config.hpp
@@ -0,0 +1,26 @@
+/* src/config.hpp.  Generated from config.hpp.in by configure.  */
+/* src/config.hpp.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "vpsolver"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "fdabrandao@dcc.fc.up.pt"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "VPSolver"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "VPSolver 3.0.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "vpsolver"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "http://vpsolver.dcc.fc.up.pt/"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "3.0.0"
+
+/* Version number of package */
+#define VERSION "3.0.0"
