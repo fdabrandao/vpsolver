88acf55e95d30aed342ab3f9dbbc0d7fada5b254
Filipe Brandao <fdabrandao@gmail.com>
2016-01-25 03:28:37 +0000

    Use transitivity to reduce the number of loss arcs

--- src/arcflow.cpp
+++ src/arcflow.cpp
@@ -353,18 +353,47 @@ void Arcflow::final_compression_step(){
 
 void Arcflow::finalize(){
     assert(ready == false);
+
     S = 0;
     Ts.clear();
     for(int i = 0; i < nbtypes; i++)
         Ts.push_back(NS.size()+i);
-    for(int i = 0; i < (int)Ts.size(); i++)
+    for(int i = 0; i < nbtypes; i++)
         A.push_back(Arc(Ts[i], S, nsizes));
-    for(int i = 1; i < NS.size(); i++){
+
+    vector<vector<int> > bigger_than(nbtypes);
+    for(int t1 = 0; t1 < nbtypes; t1++)
+        for(int t2 = 0; t2 < nbtypes; t2++)
+            if(t1 != t2 && is_valid(Ws[t1], Ws[t2]))
+                if(Ws[t1] != Ws[t2] || (t1 < t2 && Ws[t1] == Ws[t2]))
+                    bigger_than[t1].push_back(t2);
+
+    vector<bool> valid_tgts(nbtypes);
+    for(int i = 1; i < (int)NS.size(); i++){
         const vector<int> &u = NS.get_label(i);
-        for(int j = 0; j < (int)Ts.size(); j++)
-            if(is_valid(u, Ws[j]))
-                A.push_back(Arc(i, Ts[j], nsizes));
+        for(int t = 0; t < nbtypes; t++)
+            valid_tgts[t] = is_valid(u, Ws[t]);
+        for(int t1 = 0; t1 < nbtypes; t1++)
+            if(valid_tgts[t1])
+                for(int t2 : bigger_than[t1]) valid_tgts[t2] = false;
+        for(int t = 0; t < nbtypes; t++)
+            if(valid_tgts[t])
+                A.push_back(Arc(i, Ts[t], nsizes));
     }
+
+    for(int t1 = 0; t1 < nbtypes; t1++){
+        valid_tgts.assign(nbtypes, false);
+        for(int t2 : bigger_than[t1])
+            valid_tgts[t2] = true;
+        for(int t2 : bigger_than[t1])
+            if(valid_tgts[t2])
+                for(int t3 : bigger_than[t2])
+                    valid_tgts[t3] = false;
+        for(int t2 : bigger_than[t1])
+            if(valid_tgts[t2])
+                A.push_back(Arc(Ts[t1], Ts[t2], nsizes));
+    }
+
     ready = true;
 }
 
