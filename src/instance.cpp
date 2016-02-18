177ecf493241eee99e3d93db3d65a3215822afca
Filipe Brandao <fdabrandao@gmail.com>
2016-02-18 14:23:13 +0000

    Fix compatibility issues with clang on Mac OS

--- src/instance.cpp
+++ src/instance.cpp
@@ -19,6 +19,7 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **/
 #include <cstring>
+#include <cstdlib>
 #include <cmath>
 #include <vector>
 #include <algorithm>
